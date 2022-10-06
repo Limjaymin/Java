685957cac8f734777c938a79c21b3a6d55361d27
Limjaymin <111054617+Limjaymin@users.noreply.github.com>
2022-10-06 21:38:11 +0900

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,12 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] a1 = new int[3];
+		for(int n : a1) {
+			System.out.println(n);
+		}
+	}
+
+}
