af69664b881ae67248441eae07bf453e769ded0e
Limjaymin <111054617+Limjaymin@users.noreply.github.com>
2023-01-01 15:21:37 +0900

    230101

--- Test01.java
+++ Test01.java
@@ -8,7 +8,7 @@ public class Test01 {
 		// 국어,영어,수학,미술,체육 점수를 입력하세요.(0~100), 배열에 저장하고 (점수 저장, 과목배열 초기값)
 		int intArray[] = new int[5];
  		int sum=0;
-		
+		// scanner 사용
 		Scanner scanner = new Scanner(System.in);
 		String names[] = {"국어점수", "영어점수", "수학점수", "미술점수", "체육점수"};
 		
